043db0f0d647e1de4b553f762f4757f2a992036f
Alexandre-JAMROZ <alexandrejamrozpro@gmail.com>
2026-02-15 20:58:28 +1100

    ajout: exercice lab et workshop en react

---
+++ 3-Front_End_Development_Libraries/1-React_Fundamentals/Lab/1-reusable-footer.jsx
@@ -0,0 +1,54 @@
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-section">
+                <ul className="list">
+                    <li className="list-item">
+                        <a href="#">GitHub</a>
+                    </li>
+                    <li className="list-item">
+                        <a href="#">LinkedIn</a>
+                    </li>
+                </ul>
+
+                <ul className="list">
+                    <li className="list-item">
+                        <a href="#">Instagram</a>
+                    </li>
+                    <li className="list-item">
+                        <a href="#">Facebook</a>
+                    </li>
+                </ul>
+
+                <ul className="list">
+                    <li className="list-item">
+                        <a href="#">Steam</a>
+                    </li>
+                    <li className="list-item">
+                        <a href="#">FreeCodeCamp</a>
+                    </li>
+                </ul>
+            </div>
+
+            <p className="copyright">© 2026 - Alexandre JAMROZ</p>
+
+            <div className="footer-icons">
+                <a className="anchor" href="#">
+                    🎮
+                </a>
+                <a className="anchor" href="#">
+                    🥊
+                </a>
+                <a className="anchor" href="#">
+                    ♟️
+                </a>
+                <a className="anchor" href="#">
+                    🎃
+                </a>
+                <a className="anchor" href="#">
+                    🍔
+                </a>
+            </div>
+        </footer>
+    );
+};
