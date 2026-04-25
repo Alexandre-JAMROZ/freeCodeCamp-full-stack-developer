43c88b5aa70a1fad7420071b0c0df5a9177e193d
Alexandre-JAMROZ <alexandrejamrozpro@gmail.com>
2026-04-25 13:56:48 +1100

    add: 3 exercices variables et strings en javascript

---
+++ 2-JavaScript/1-Variables_and_Strings/Lab/1-trivia-bot.js
@@ -0,0 +1,27 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Jarvis";
+let botLocation = "New Caledonia";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+    "In " + favoriteLanguage + ", 'true + true === 2' but 'true !== 1'.";
+console.log(codingFact);
+
+codingFact =
+    "In " + favoriteLanguage + ", The type of NaN (not a number) is 'Number'.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "'s real name is ECMAScript.";
+console.log(codingFact);
+
+console.log(
+    "It was fun sharing these facts with you. Goodbye! - " +
+        botName +
+        " from " +
+        botLocation +
+        ".",
+);
---
+++ 2-JavaScript/1-Variables_and_Strings/Lab/2-sentence-maker.js
@@ -0,0 +1,56 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "beautiful";
+noun = "dog";
+verb = "barking";
+place = "park";
+adjective2 = "hungry";
+noun2 = "cucumbers";
+
+const firstStory =
+    "Once upon a time, there was a(n) " +
+    adjective +
+    " " +
+    noun +
+    " who loved to eat " +
+    noun2 +
+    ". The " +
+    noun +
+    " lived in a " +
+    place +
+    " and had " +
+    adjective2 +
+    " nostrils that blew fire when it was " +
+    verb +
+    ".";
+console.log("First story: " + firstStory);
+
+adjective = "dreadful";
+noun = "robot";
+verb = "walking";
+place = "junkyard";
+adjective2 = "weak";
+noun2 = "computers";
+
+const secondStory =
+    "Once upon a time, there was a(n) " +
+    adjective +
+    " " +
+    noun +
+    " who loved to eat " +
+    noun2 +
+    ". The " +
+    noun +
+    " lived in a " +
+    place +
+    " and had " +
+    adjective2 +
+    " nostrils that blew fire when it was " +
+    verb +
+    ".";
+console.log("Second story: " + secondStory);
